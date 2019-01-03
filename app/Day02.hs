176e96b251cceaacb53b2840e2783265460847b0
(cdep)illabout <cdep.illabout@gmail.com>
2019-01-03 13:06:54 +0900

    Completed day 2 part 1

--- app/Day02.hs
+++ app/Day02.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Control.Lens
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
+import Data.Char (ord)
 import Data.Map (Map, insertWith, toList)
 import Data.Text (Text)
 import qualified Data.Text as Text
@@ -14,7 +15,7 @@ import Lib (someFunc)
 
 main :: IO ()
 main =
-  interact mySolution
+  interact mySolutionPart2
 
 findAllCounts :: String -> Map Char Int
 findAllCounts = foldr (\a -> insertWith (+) a 1) mempty
@@ -35,7 +36,76 @@ totalCount is =
   let (totalTwo, totalThree) = foldr (\(two, three) (accTwo, accThree) -> (accTwo + two, accThree + three)) (0, 0) is
   in totalTwo * totalThree
 
-mySolution :: String -> String
-mySolution inputStr =
+mySolutionPart1 :: String -> String
+mySolutionPart1 inputStr =
   let l = lines inputStr
   in show $ totalCount $ fmap getCounts l
+
+isSimChar :: Char -> Char -> Sim
+isSimChar c d | c == d = Same
+              | otherwise = Diff
+
+data Sim = Diff | Same deriving Eq
+
+isSimilar :: String -> String -> Bool
+isSimilar str str' = oneDiff $ zipWith isSimChar str str'
+
+oneDiff :: [Sim] -> Bool
+oneDiff sims = length (filter (== Diff) sims) == 1
+
+-- |
+--
+-- >>> findSimilar' "hello" ["badje", "iello"]
+-- Just "iello"
+--
+-- >>> findSimilar' "hello" ["gello", "dodii"]
+-- Just "gello"
+--
+-- >>> findSimilar' "hello" ["hello", "byeee"]
+-- Nothing
+--
+-- >>> findSimilar' "hello" ["toooo", "byeee"]
+-- Nothing
+findSimilar' :: String -> [String] -> Maybe String
+findSimilar' _ [] = Nothing
+findSimilar' str (h:ts) = if isSimilar str h then Just h else findSimilar' str ts
+
+-- |
+--
+-- >>> removeDiffChar "hello" "gello"
+-- "ello"
+--
+-- >>> removeDiffChar "hello" "hdllo"
+-- "hllo"
+removeDiffChar :: String -> String -> String
+removeDiffChar [] [] = []
+removeDiffChar (h:ts) (h':ts')
+  | h == h' = h : removeDiffChar ts ts'
+  | otherwise = removeDiffChar ts ts'
+removeDiffChar _ _ = error "removeDiffChar: called with two strings of different lengths"
+
+-- |
+--
+-- >>> findSimilar ["apple", "badje", "bpple"]
+-- Just "pple"
+--
+-- >>> findSimilar ["badje", "turtl", "ttrtl"]
+-- Just "trtl"
+--
+-- >>> findSimilar ["one", "two", "aaa"]
+-- Nothing
+findSimilar :: [String] -> Maybe String
+findSimilar [] = Nothing
+findSimilar (h:ts) =
+  case findSimilar' h ts of
+    Nothing -> findSimilar ts
+    Just sim -> Just $ removeDiffChar h sim
+
+-- |
+--
+-- >>> mySolutionPart2 "apple\nradde\npbdde\nraede\noafe\n"
+-- "rade"
+mySolutionPart2 :: String -> String
+mySolutionPart2 inputStr =
+  let l = lines inputStr
+  in maybe (error "mySolutionPart2: didn't find any similar strings") id $ findSimilar l -- show $ totalCount $ fmap getCounts l
