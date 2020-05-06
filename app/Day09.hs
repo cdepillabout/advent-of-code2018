2c86465b9951a730eaf6b738aed135149b2a4c6a
(cdep)illabout <cdep.illabout@gmail.com>
2020-05-06 14:26:52 +0900

    update

--- app/Day09.hs
+++ app/Day09.hs
@@ -2,6 +2,9 @@ module Main where
 
 import ClassyPrelude
 
+import qualified Data.Sequence as Seq
+import Data.Sequence (Seq)
+
 import qualified Data.Map as Map
 import Data.Map (Map)
 
@@ -13,21 +16,22 @@ type HighScore = Int
 
 type Player = Int
 
-type Board = [Int]
+-- type Board = [Int]
+type Board = Seq Int
 
 type PrevMarbleNumIdx = Int
 type NewMarbleNum = Int
 
-insertAt :: Int -> a -> [a] -> [a]
-insertAt 0 a as = a : as
-insertAt n a (h:t) = h : insertAt (n - 1) a t
+-- insertAt :: Int -> a -> [a] -> [a]
+-- insertAt 0 a as = a : as
+-- insertAt n a (h:t) = h : insertAt (n - 1) a t
 
 type NewMarbleIdx = Int
 
 insertMarble :: PrevMarbleNumIdx -> NewMarbleNum -> Board -> (NewMarbleIdx, Board)
 insertMarble prevMarbleNumIdx newMarbleNum board =
   let indexOfNext = (prevMarbleNumIdx + 2) `mod` length board
-  in (indexOfNext, insertAt indexOfNext newMarbleNum board)
+  in (indexOfNext, Seq.insertAt indexOfNext newMarbleNum board)
 
 type Score = Int
 type Scores = Map Player Score
@@ -36,9 +40,12 @@ type NewCurrentMarble = Int
 
 type MarbleRemovedNum = Int
 
+-- removeIndex :: Int -> Board -> (MarbleRemovedNum, Board)
+-- removeIndex 0 (h:t) = (h, t)
+-- removeIndex n (h:t) = let (ff, tt) = removeIndex (n - 1) t in (ff, h:tt)
+
 removeIndex :: Int -> Board -> (MarbleRemovedNum, Board)
-removeIndex 0 (h:t) = (h, t)
-removeIndex n (h:t) = let (ff, tt) = removeIndex (n - 1) t in (ff, h:tt)
+removeIndex i b = (Seq.index b i, Seq.deleteAt i b)
 
 updateScores :: Player -> Score -> Scores -> Scores
 updateScores p s scores = insertWith (+) p s scores
@@ -69,11 +76,11 @@ do23 players scores board prevMarbleNumIdx newMarbleNum =
   (newScores, newBoard, marbleToRemoveIdx)
 
 createBoard :: Players -> Marbles -> (Scores, Board)
-createBoard players lastMarble = go Map.empty [0] 0 1
+createBoard players lastMarble = go Map.empty (Seq.singleton 0) 0 1
   where
   go :: Scores -> Board -> PrevMarbleNumIdx -> NewMarbleNum -> (Scores, Board)
-  go scores board 0 1 = go scores [0,1] 1 2
-  go scores board prevMarbleNumIdx newMarbleNum
+  go scores _ 0 1 = go scores (0 Seq.<| Seq.singleton 1) 1 2
+  go scores !board !prevMarbleNumIdx !newMarbleNum
     | newMarbleNum > lastMarble = (scores, board)
     | newMarbleNum `mod` 23 == 0 =
         let (newScores, newBoard, newCurrentMarbleIdx) =
@@ -89,18 +96,15 @@ createBoard players lastMarble = go Map.empty [0] 0 1
           newMarbleIdx
           (newMarbleNum + 1)
 
--- simGame :: Players -> Marbles -> HighScore
--- simGame players lastMarble = go [0] 1
---   where
---   go :: [Int] -> Player -> HighScore
---   go board ps | ps == players = board
---   go board 0 = go [0]
-
 main :: IO ()
 main = do
-  let players = 9
-      lastMarble = 25
+  -- let players = 9
+  --     lastMarble = 25
+  let players = 424
+      lastMarble = 7148200
+  -- let players = 405
+  --     lastMarble = 71700
       -- highScore = simGame [0]
-      board = createBoard players lastMarble
-  -- print highScore
+      (scores, _board) = createBoard players lastMarble
+  print (lastEx $ sort $ Map.elems scores)
   pure ()
